2fba678b03f3c8285b76882992e85377f68dae4f
acruz2332 <akbar.fajar2301@mail.ugm.ac.id>
2022-10-22 19:14:06 +0700

    pertemuan8

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,11 +7,9 @@ use App\Models\Post;
 use timestamps;
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct(){
+        $this->middleware('auth');
+    }
     public function index()
     {
         $data = array(
